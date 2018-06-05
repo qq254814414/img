eae47c228b399f1d41dc42768055093083c7afb3
ssdr <2548414414@qq.com>
2018-06-05 17:52:37 +0800

    update

--- app/src/main/java/com/mengxin/img/ui/fragment/AuthorFragment.java
+++ app/src/main/java/com/mengxin/img/ui/fragment/AuthorFragment.java
@@ -54,6 +54,8 @@ public class AuthorFragment extends Fragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.author,container,false);
+
+
         mSubscriptions = new CompositeDisposable();
         manager = getActivity().getSupportFragmentManager();
         initView(view);
